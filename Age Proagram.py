059ed68d63c9f2b1247311f6158705c2cf77cefc
AadeeshJainB2K <aadeeshjain15@gmail.com>
2025-09-05 00:12:56 +0530

    added very old codes which were before i learnt vcs

---
+++ Age Proagram.py
@@ -0,0 +1,5 @@
+Age = int(input ("What_is_your_age?"))
+if Age >=18 :
+    print ("Your are an Adiult You Can Vote")
+elif Age < 18 :
+    print ("You are in school")
---
+++ Calculator.py
@@ -0,0 +1,17 @@
+a = (int(input("Enter First Number")))
+b = (input("Enter Opreation Which You Want To Do ?"))
+c = (int(input("Enter Second Number")))
+if b == "+":
+    d = a + c
+    print(d)
+elif b == "-":
+    e = a - c
+    print(e)
+elif b == "*":
+    f = a * c
+    print(f)
+elif b == "/":
+    g = a / c
+    print(g)
+else:
+    print("Not Define")
