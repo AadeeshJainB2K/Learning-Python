745d295de18f51fe1b91d7fcb2ee81b9a793750b
Aadeesh Jain <aadeeshjain15@gmail.com>
2025-11-21 19:55:22 +0530

    +1

---
+++ Practice Questions/Practice Question 1/Q1.py
@@ -0,0 +1,4 @@
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+sum = float(a) + float(b)
+print("First number ", a , " Second number " , b , " Sum is " , sum)
--- Rough/Rough.py
+++ Rough/Rough.py
@@ -1,41 +1,15 @@
-def get_numeric_input(prompt):
-    #User se ek numeric value leta hai aur galat input ko handle karta hai."""
-    while True:
-        try:
-            return float(input(prompt))
-        except ValueError:
-            print("Invalid input. Please enter a numeric value.")
+name = '"Aadeesh Jain"'
+academiClass = 11 
+school = "Springfield High"
+age = 15
+age2 = None
+isOld = False
 
-def get_operation():
-    #User se ek valid operation leta hai.
-    valid_operations = ["+", "-", "*", "/"]
-    while True:
-        op = input(f"Enter Operation ({', '.join(valid_operations)}): ")
-        if op in valid_operations:
-            return op
-        else:
-            print("Invalid Operation. Please choose from the allowed options.")
+print("My name is : " , name , "I study in class : " , academiClass , "at" , school , "and my age is : " , age)
+print(type(name))
+print(type(academiClass))
+print(type(school))
+print(type(age))
+print(type(age2))
+print(type(isOld))
 
-def main():
-    #Calculator chalaane ke liye yeh main function hai.
-    first_number = get_numeric_input("Enter First Number: ")
-    operation = get_operation()
-    second_number = get_numeric_input("Enter Second Number: ")
-
-    if operation == "+":
-        result = first_number + second_number
-    elif operation == "-":
-        result = first_number - second_number
-    elif operation == "*":
-        result = first_number * second_number
-    elif operation == "/":
-        if second_number == 0:
-            print("Error: Division by zero is not defined")
-            return  # Function se bahar nikal jaa
-        else:
-            result = first_number / second_number
-
-    print(f"Output is {result}")
-
-if __name__ == "__main__":
-    main()
