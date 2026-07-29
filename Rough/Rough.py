f11623dd08831aff5d0f66d33094ef31bdeabe05
Aadeesh Jain <aadeeshjain15@gmail.com>
2026-07-29 23:38:02 +0530

    just solving questions at 11:37

--- Rough/Rough.py
+++ Rough/Rough.py
@@ -1,5 +1,18 @@
-s =  input("Enter a string to check if it is a palindrome :")
-if (s==s[::-1]):
-    print("The string is a palindrome")
-else:
-    print("The string is not a palindrome")
+def safe_array_divider(L1, L2):
+    r = []
+    for i in range(max(len(L1),len(L2))):
+        try:
+            r.append(L1[i]/L2[i])
+        except ZeroDivisionError:
+            print("L2 got a 0 at" , i , "index")
+            print("Div by Zero")
+            break
+        except IndexError :
+            print("unequal lengths")
+            break
+        else: isSucessful = True
+    else:
+        print(r)
+
+safe_array_divider([1,2,3],[1,2,3])
+safe_array_divider([1,2,3],[1,2,3,4])
